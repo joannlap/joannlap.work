b85575a26892c26ed0a0fb955441f05ed445def3
Jo-Ann Lieuw <joannlieuw@gmail.com>
2019-12-29 19:08:24 +0100

    first commit

--- ja.js
+++ ja.js
@@ -11,10 +11,9 @@ $(document).ready(function () {
             var hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 500, function () {
+            }, 400, function () {
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
